5943b565345e5d1f2ac926b73410eb7855f66c32
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-01-25 16:49:56 +0100

    Update : ajout champ offre_salaire

--- TRAVAIL/BDD/amd_web.sql
+++ TRAVAIL/BDD/amd_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 25 jan. 2023 à 14:26
+-- Généré le : mer. 25 jan. 2023 à 15:47
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -122,10 +122,11 @@ INSERT INTO `droits` (`droit_id`, `droit_nom`) VALUES
 DROP TABLE IF EXISTS `offre`;
 CREATE TABLE IF NOT EXISTS `offre` (
   `offre_id` int NOT NULL AUTO_INCREMENT COMMENT 'Identifiant de l''offre',
-  `offre_description` text NOT NULL COMMENT 'Description de l''offre',
-  `offre_titre` varchar(255) NOT NULL COMMENT 'Titre de l''offre',
+  `offre_titre` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT 'Titre de l''offre',
+  `offre_description` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT 'Description de l''offre',
+  `offre_salaire` int NOT NULL COMMENT 'Salaire de l''offre',
+  `offre_siret` char(14) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT 'Numéro de SIRET de l''entreprise',
   `offre_adresse` text COMMENT 'Adresse de l''offre(num + nom de rue)',
-  `offre_siret` char(14) DEFAULT NULL COMMENT 'Numéro de SIRET de l''entreprise',
   `offre_date_creation` datetime NOT NULL COMMENT 'Date de création de l''offre',
   `offre_date_modification` datetime DEFAULT NULL COMMENT 'Date de modification de l''offre',
   `offre_utilisateur_id` int NOT NULL COMMENT 'identifiant de l''utilisateur',
@@ -139,19 +140,19 @@ CREATE TABLE IF NOT EXISTS `offre` (
 -- Déchargement des données de la table `offre`
 --
 
-INSERT INTO `offre` (`offre_id`, `offre_description`, `offre_titre`, `offre_adresse`, `offre_siret`, `offre_date_creation`, `offre_date_modification`, `offre_utilisateur_id`, `offre_ville_id`) VALUES
-(1, 'Recherche développeur secteur strasbourg en contrat cdi.\r\ntemps de travail 35h par semaine.', 'Développeur web', '25 rue des champs', NULL, '2023-01-18 22:46:17', '2023-01-18 22:46:17', 2, 2),
-(2, 'En tant que développeur backend, vous participerez à toutes les phases de la création et développement d\'une architecture back pour nos applications web et mobile. ', 'Recherche Développeur - Fullstack', NULL, NULL, '2023-01-25 13:34:30', '0000-00-00 00:00:00', 5, 1),
-(3, 'Rattaché(e) à votre manager, dans le cadre de nos projets IT, vous intégrez une équipe qui sera chargée de réécrire l’ensemble de nos applications basées sur une architecture services dans un environnement Microsoft.\r\n\r\nVous intervenez sur le développement des applications web :\r\n\r\n    Développement des fonctionnalités selon les spécifications techniques\r\n    Développement d’API REST & restitution des données dans les applications Angular\r\n    Optimisation des performances des API & des bases de données\r\n    Développement des interfaces des applications Angular\r\n', 'Développeur Full Stack H/F', NULL, NULL, '2023-01-25 13:46:36', NULL, 5, 1),
-(4, 'Rattaché(e) à votre manager, dans le cadre de nos projets IT, vous intégrez une équipe qui sera chargée de réécrire l’ensemble de nos applications basées sur une architecture services dans un environnement Microsoft.\r\n\r\nVous intervenez sur le développement ergonomique des applications Angular :\r\n\r\n    Développement de nouveaux composants de l’UI Kit\r\n    Restitution des données des API avec les composants de l’UI Kit\r\n    Participation à l’élaboration de l’ergonomie des applications\r\n', 'Développeur Front-End H/F', NULL, NULL, '2023-01-25 13:50:28', NULL, 5, 1),
-(5, 'Dans le cadre du développement de notre activité, sous la responsabilité du Chef de Projets, nous recherchons notre futur(e) Développeur / Intégrateur WordPress Web (H/F),', 'Développeur / Intégrateur site Web (H/F)', NULL, NULL, '2023-01-25 13:52:21', NULL, 2, 2),
-(6, 'Dans le cadre d’un important projet avec une entreprise grand compte, nous recherchons des Développeurs en environnement WINDEV.\r\n', 'Développeur WINDEV', NULL, NULL, '2023-01-25 13:52:21', NULL, 5, 1),
-(7, 'Notre agence de Colmar recherche ses futurs développeurs Web !\r\nVous intégrerez une équipe innovante de 10 consultants, accompagnée de développeurs expérimentés .', 'Développeur PHP/Symfony (H/F)', NULL, NULL, '2023-01-25 13:58:59', NULL, 2, 2),
-(8, 'Intégrez un éditeur de logiciels leader sur son marché !\r\nNous recherchons un Développeur FullStack .NET / Angular pour accompagner la croissance d’un éditeur spécialisé. Notre client est un éditeur de logiciels unique sur le marché en concevant des solutions innovantes à destination des agences d’intérim. Le poste est à pourvoir en CDI proche de Colmar. ', 'Développeur FullStack Angular / .NET H/F', NULL, NULL, '2023-01-25 14:02:58', NULL, 2, 5),
-(9, 'Spécialiste de la formation en Digital Learning, recherche pour son entreprise partenaire, une société spécialisée dans les solutions web mobile, un(e) Développeur(se) Web en contrat d\'apprentissage, pour préparer l’une de nos formations diplômantes reconnues par l\'Etat de niveau 5 à niveau 7 (Bac+2, Bachelor/Bac+3 ou Mastère/Bac+5). ', 'Alternance Développeur(se) Web & mobile - Maisons-Alfort (F/H)', NULL, NULL, '2023-01-25 14:05:44', NULL, 2, 8),
-(10, 'Dans le cadre du développement de nos activités d’agence webmarketing, nous recherchons un développeur web pour un poste en CDI afin de compléter nos effectifs.', 'Développeur web, PHP, HTML, JAVASCRIPT, WordPress, Prestashop', NULL, NULL, '2023-01-25 14:05:44', NULL, 2, 4),
-(11, 'Dans le cadre d\'une équipe de développement existante, vous participerez aux développements de nos applications actuelles et futures.\r\nVous serez en charge de corriger et d\'améliorer nos applications et serez amener à participer à l\'analyse et à la conception de nos projets.', 'Développeur Web PHP ou équivalent H/F', NULL, NULL, '2023-01-25 14:12:16', NULL, 2, 7),
-(12, 'Dans le cadre de notre croissance, nous recrutons un(e) Développeur(se) PHP F/H . Intégré(e) au sein de notre équipe, vous pourrez mettre en pratique vos acquis, développer de nouvelles compétences techniques et fonctionnelles. ', 'Développeur(se) PHP (H/F)', NULL, NULL, '2023-01-25 14:12:16', NULL, 2, 10);
+INSERT INTO `offre` (`offre_id`, `offre_titre`, `offre_description`, `offre_salaire`, `offre_siret`, `offre_adresse`, `offre_date_creation`, `offre_date_modification`, `offre_utilisateur_id`, `offre_ville_id`) VALUES
+(1, 'Développeur web', 'Recherche développeur secteur strasbourg en contrat cdi.\r\ntemps de travail 35h par semaine.', 2200, NULL, '25 rue des champs', '2023-01-18 22:46:17', '2023-01-18 22:46:17', 2, 2),
+(2, 'Recherche Développeur - Fullstack', 'En tant que développeur backend, vous participerez à toutes les phases de la création et développement d\'une architecture back pour nos applications web et mobile. ', 2700, NULL, NULL, '2023-01-25 13:34:30', '0000-00-00 00:00:00', 5, 1),
+(3, 'Développeur Full Stack H/F', 'Rattaché(e) à votre manager, dans le cadre de nos projets IT, vous intégrez une équipe qui sera chargée de réécrire l’ensemble de nos applications basées sur une architecture services dans un environnement Microsoft.\r\n\r\nVous intervenez sur le développement des applications web :\r\n\r\n    Développement des fonctionnalités selon les spécifications techniques\r\n    Développement d’API REST & restitution des données dans les applications Angular\r\n    Optimisation des performances des API & des bases de données\r\n    Développement des interfaces des applications Angular\r\n', 2850, NULL, NULL, '2023-01-25 13:46:36', NULL, 5, 1),
+(4, 'Développeur Front-End H/F', 'Rattaché(e) à votre manager, dans le cadre de nos projets IT, vous intégrez une équipe qui sera chargée de réécrire l’ensemble de nos applications basées sur une architecture services dans un environnement Microsoft.\r\n\r\nVous intervenez sur le développement ergonomique des applications Angular :\r\n\r\n    Développement de nouveaux composants de l’UI Kit\r\n    Restitution des données des API avec les composants de l’UI Kit\r\n    Participation à l’élaboration de l’ergonomie des applications\r\n', 2300, NULL, NULL, '2023-01-25 13:50:28', NULL, 5, 1),
+(5, 'Développeur / Intégrateur site Web (H/F)', 'Dans le cadre du développement de notre activité, sous la responsabilité du Chef de Projets, nous recherchons notre futur(e) Développeur / Intégrateur WordPress Web (H/F),', 2800, NULL, NULL, '2023-01-25 13:52:21', NULL, 2, 2),
+(6, 'Développeur WINDEV', 'Dans le cadre d’un important projet avec une entreprise grand compte, nous recherchons des Développeurs en environnement WINDEV.\r\n', 3000, NULL, NULL, '2023-01-25 13:52:21', NULL, 5, 1),
+(7, 'Développeur PHP/Symfony (H/F)', 'Notre agence de Colmar recherche ses futurs développeurs Web !\r\nVous intégrerez une équipe innovante de 10 consultants, accompagnée de développeurs expérimentés .', 2750, NULL, NULL, '2023-01-25 13:58:59', NULL, 2, 2),
+(8, 'Développeur FullStack Angular / .NET H/F', 'Intégrez un éditeur de logiciels leader sur son marché !\r\nNous recherchons un Développeur FullStack .NET / Angular pour accompagner la croissance d’un éditeur spécialisé. Notre client est un éditeur de logiciels unique sur le marché en concevant des solutions innovantes à destination des agences d’intérim. Le poste est à pourvoir en CDI proche de Colmar. ', 3083, NULL, NULL, '2023-01-25 14:02:58', NULL, 2, 5),
+(9, 'Alternance Développeur(se) Web & mobile - Maisons-Alfort (F/H)', 'Spécialiste de la formation en Digital Learning, recherche pour son entreprise partenaire, une société spécialisée dans les solutions web mobile, un(e) Développeur(se) Web en contrat d\'apprentissage, pour préparer l’une de nos formations diplômantes reconnues par l\'Etat de niveau 5 à niveau 7 (Bac+2, Bachelor/Bac+3 ou Mastère/Bac+5). ', 2222, NULL, NULL, '2023-01-25 14:05:44', NULL, 2, 8),
+(10, 'Développeur web, PHP, HTML, JAVASCRIPT, WordPress, Prestashop', 'Dans le cadre du développement de nos activités d’agence webmarketing, nous recherchons un développeur web pour un poste en CDI afin de compléter nos effectifs.', 2300, NULL, NULL, '2023-01-25 14:05:44', NULL, 2, 4),
+(11, 'Développeur Web PHP ou équivalent H/F', 'Dans le cadre d\'une équipe de développement existante, vous participerez aux développements de nos applications actuelles et futures.\r\nVous serez en charge de corriger et d\'améliorer nos applications et serez amener à participer à l\'analyse et à la conception de nos projets.', 2450, NULL, NULL, '2023-01-25 14:12:16', NULL, 2, 7),
+(12, 'Développeur(se) PHP (H/F)', 'Dans le cadre de notre croissance, nous recrutons un(e) Développeur(se) PHP F/H . Intégré(e) au sein de notre équipe, vous pourrez mettre en pratique vos acquis, développer de nouvelles compétences techniques et fonctionnelles. ', 2500, NULL, NULL, '2023-01-25 14:12:16', NULL, 2, 10);
 
 -- --------------------------------------------------------
 
