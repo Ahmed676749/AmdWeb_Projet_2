e19ab7d8ecfb3cff994cdd51e73202d8d5670a34
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-01-25 15:31:49 +0100

    Update amd_web.sql
    Alimentation des tables categorie, droits, offre, utilisateur et ville de la BDD

--- TRAVAIL/BDD/amd_web.sql
+++ TRAVAIL/BDD/amd_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 25 jan. 2023 à 12:31
+-- Généré le : mer. 25 jan. 2023 à 14:26
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -76,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `categorie` (
   `categorie_nom` varchar(255) NOT NULL COMMENT 'Nom de la catégorie',
   `categorie_sous_categorie_id` int NOT NULL,
   PRIMARY KEY (`categorie_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `categorie`
@@ -86,7 +86,8 @@ INSERT INTO `categorie` (`categorie_id`, `categorie_nom`, `categorie_sous_catego
 (1, 'CDI', 0),
 (2, 'Interim', 0),
 (3, 'CDD', 0),
-(4, 'Alternance', 0);
+(4, 'Alternance', 0),
+(5, 'Type de contrat', 1);
 
 -- --------------------------------------------------------
 
@@ -99,15 +100,18 @@ CREATE TABLE IF NOT EXISTS `droits` (
   `droit_id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'identifiant de droits',
   `droit_nom` varchar(20) NOT NULL COMMENT 'nom des droits',
   PRIMARY KEY (`droit_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `droits`
 --
 
 INSERT INTO `droits` (`droit_id`, `droit_nom`) VALUES
-(1, 'postuler'),
-(2, 'déposer');
+(1, 'CANDIDAT'),
+(2, 'RECRUTEUR'),
+(3, 'ADMINISTRATEUR'),
+(4, 'MODERATEUR'),
+(5, 'VISITEUR');
 
 -- --------------------------------------------------------
 
@@ -129,14 +133,25 @@ CREATE TABLE IF NOT EXISTS `offre` (
   PRIMARY KEY (`offre_id`),
   KEY `of_utilisateur_id` (`offre_utilisateur_id`),
   KEY `of_ville_id` (`offre_ville_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `offre`
 --
 
 INSERT INTO `offre` (`offre_id`, `offre_description`, `offre_titre`, `offre_adresse`, `offre_siret`, `offre_date_creation`, `offre_date_modification`, `offre_utilisateur_id`, `offre_ville_id`) VALUES
-(1, 'Recherche développeur secteur strasbourg en contrat cdi.\r\ntemps de travail 35h par semaine.', 'Développeur web', '25 rue des champs', NULL, '2023-01-18 22:46:17', '2023-01-18 22:46:17', 2, 1);
+(1, 'Recherche développeur secteur strasbourg en contrat cdi.\r\ntemps de travail 35h par semaine.', 'Développeur web', '25 rue des champs', NULL, '2023-01-18 22:46:17', '2023-01-18 22:46:17', 2, 2),
+(2, 'En tant que développeur backend, vous participerez à toutes les phases de la création et développement d\'une architecture back pour nos applications web et mobile. ', 'Recherche Développeur - Fullstack', NULL, NULL, '2023-01-25 13:34:30', '0000-00-00 00:00:00', 5, 1),
+(3, 'Rattaché(e) à votre manager, dans le cadre de nos projets IT, vous intégrez une équipe qui sera chargée de réécrire l’ensemble de nos applications basées sur une architecture services dans un environnement Microsoft.\r\n\r\nVous intervenez sur le développement des applications web :\r\n\r\n    Développement des fonctionnalités selon les spécifications techniques\r\n    Développement d’API REST & restitution des données dans les applications Angular\r\n    Optimisation des performances des API & des bases de données\r\n    Développement des interfaces des applications Angular\r\n', 'Développeur Full Stack H/F', NULL, NULL, '2023-01-25 13:46:36', NULL, 5, 1),
+(4, 'Rattaché(e) à votre manager, dans le cadre de nos projets IT, vous intégrez une équipe qui sera chargée de réécrire l’ensemble de nos applications basées sur une architecture services dans un environnement Microsoft.\r\n\r\nVous intervenez sur le développement ergonomique des applications Angular :\r\n\r\n    Développement de nouveaux composants de l’UI Kit\r\n    Restitution des données des API avec les composants de l’UI Kit\r\n    Participation à l’élaboration de l’ergonomie des applications\r\n', 'Développeur Front-End H/F', NULL, NULL, '2023-01-25 13:50:28', NULL, 5, 1),
+(5, 'Dans le cadre du développement de notre activité, sous la responsabilité du Chef de Projets, nous recherchons notre futur(e) Développeur / Intégrateur WordPress Web (H/F),', 'Développeur / Intégrateur site Web (H/F)', NULL, NULL, '2023-01-25 13:52:21', NULL, 2, 2),
+(6, 'Dans le cadre d’un important projet avec une entreprise grand compte, nous recherchons des Développeurs en environnement WINDEV.\r\n', 'Développeur WINDEV', NULL, NULL, '2023-01-25 13:52:21', NULL, 5, 1),
+(7, 'Notre agence de Colmar recherche ses futurs développeurs Web !\r\nVous intégrerez une équipe innovante de 10 consultants, accompagnée de développeurs expérimentés .', 'Développeur PHP/Symfony (H/F)', NULL, NULL, '2023-01-25 13:58:59', NULL, 2, 2),
+(8, 'Intégrez un éditeur de logiciels leader sur son marché !\r\nNous recherchons un Développeur FullStack .NET / Angular pour accompagner la croissance d’un éditeur spécialisé. Notre client est un éditeur de logiciels unique sur le marché en concevant des solutions innovantes à destination des agences d’intérim. Le poste est à pourvoir en CDI proche de Colmar. ', 'Développeur FullStack Angular / .NET H/F', NULL, NULL, '2023-01-25 14:02:58', NULL, 2, 5),
+(9, 'Spécialiste de la formation en Digital Learning, recherche pour son entreprise partenaire, une société spécialisée dans les solutions web mobile, un(e) Développeur(se) Web en contrat d\'apprentissage, pour préparer l’une de nos formations diplômantes reconnues par l\'Etat de niveau 5 à niveau 7 (Bac+2, Bachelor/Bac+3 ou Mastère/Bac+5). ', 'Alternance Développeur(se) Web & mobile - Maisons-Alfort (F/H)', NULL, NULL, '2023-01-25 14:05:44', NULL, 2, 8),
+(10, 'Dans le cadre du développement de nos activités d’agence webmarketing, nous recherchons un développeur web pour un poste en CDI afin de compléter nos effectifs.', 'Développeur web, PHP, HTML, JAVASCRIPT, WordPress, Prestashop', NULL, NULL, '2023-01-25 14:05:44', NULL, 2, 4),
+(11, 'Dans le cadre d\'une équipe de développement existante, vous participerez aux développements de nos applications actuelles et futures.\r\nVous serez en charge de corriger et d\'améliorer nos applications et serez amener à participer à l\'analyse et à la conception de nos projets.', 'Développeur Web PHP ou équivalent H/F', NULL, NULL, '2023-01-25 14:12:16', NULL, 2, 7),
+(12, 'Dans le cadre de notre croissance, nous recrutons un(e) Développeur(se) PHP F/H . Intégré(e) au sein de notre équipe, vous pourrez mettre en pratique vos acquis, développer de nouvelles compétences techniques et fonctionnelles. ', 'Développeur(se) PHP (H/F)', NULL, NULL, '2023-01-25 14:12:16', NULL, 2, 10);
 
 -- --------------------------------------------------------
 
@@ -176,16 +191,19 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   PRIMARY KEY (`utilisateur_id`),
   KEY `ut_v_id` (`utilisateur_ville_id`),
   KEY `ut_v_id_2` (`utilisateur_ville_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`utilisateur_id`, `utilisateur_nom`, `utilisateur_prenom`, `utilisateur_adresse`, `utilisateur_type`, `utilisateur_cv`, `utilisateur_mail`, `utilisateur_mdp`, `utilisateur_date_creation`, `utilisateur_date_modification`, `utilisateur_date_connexion`, `utilisateur_ville_id`, `utilisateur_droit_id`) VALUES
-(1, 'DUPONT', 'Jean', '3 rue des champs', 'candidat', NULL, 'dupont@jean.fr', '123', '2023-01-18 22:39:08', '2023-01-18 22:39:08', '2023-01-18 22:39:08', 1, 1),
-(2, 'DUROC', 'Tournesol', '35 rue des tournesols', 'recruteur', NULL, 'tournesol@duroc.fr', '235', '2023-01-19 16:22:37', '2023-01-19 16:22:37', '2023-01-19 16:22:37', 2, 2),
-(3, 'BONNEAU', 'Jean', '11 rue de la Charcuterie', '', NULL, 'jeanbonneau@dev.fr', 'Jean*dev68', '2023-01-25 11:16:13', NULL, NULL, 0, 0);
+(1, 'DUPONT', 'Jean', '3 rue des champs', 'candidat', NULL, 'dupont@jean.fr', '123', '2023-01-18 22:39:08', '2023-01-25 22:39:08', '2023-01-18 22:39:08', 1, 1),
+(2, 'DUROC', 'Tournesol', '35 rue des tournesols', 'recruteur', NULL, 'tournesol@duroc.fr', '235', '2023-01-19 16:22:37', '2023-01-25 16:22:37', '2023-01-19 16:22:37', 2, 2),
+(3, 'BONNEAU', 'Jean', '11 rue de la Charcuterie', 'candidat', NULL, 'jeanbonneau@dev.fr', 'Jean*dev68', '2023-01-25 11:16:13', '2023-01-25 14:12:50', NULL, 6, 1),
+(4, 'CONDA', 'Anna', '7 rue de la Jungle', 'candidat', NULL, 'annaconda@amazon.br', 'Moogli*7', '2023-01-25 13:00:59', '2023-01-25 14:15:19', NULL, 11, 1),
+(5, 'SAILAIR', 'Jacques', '20 boulevard Aryton Senna', 'recruteur', NULL, 'jaksailair@surlaroute.vit', 'Porshe911', '2023-01-25 13:17:12', '2023-01-25 14:17:12', NULL, 9, 2),
+(6, 'TIME', 'Vincent', '20 rue de la Tirelire', 'visiteur', NULL, 'vincentime@poche.in', 'Moneyman*68', '2023-01-25 13:17:12', '0000-00-00 00:00:00', NULL, 8, 5);
 
 -- --------------------------------------------------------
 
