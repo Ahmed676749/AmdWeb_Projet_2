ef4f0e554ea2282ed6e671cd5de491c17658e82c
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-01-27 11:53:17 +0100

    Update fonction s'inscrire

--- inscription.php
+++ inscription.php
@@ -1,8 +1,28 @@
 <?php
+  include("connect.php");
+
   $strTitle = "AmdWeb offres d'emplois, inscription";
   $strPage = "inscription";
 
   include("header.php");
+
+  // Récupération des informations du formulaire
+  $strUserType  = $_POST['role']??'';
+  $strName      = $_POST['name']??'';
+  $strFirstname = $_POST['firstname']??'';
+  $strAddress   = $_POST['address']??'';
+  $strMail      = $_POST['email']??'';
+  $strPassword  = $_POST['password']??'';
+
+  $strRq 			= "	SELECT *
+                  FROM utilisateur";
+
+  $arrUsers 	= $db->query($strRq)->fetchAll();
+
+  var_dump($strRq);
+  var_dump($arrUsers);
+
+  var_dump($_POST);
 
 ?>
 
@@ -18,49 +38,38 @@
               Oui, l'inscription est obligatoire comme sur tous les sites.
             </p>
             <div class="form-box">
-              <form>
+              <form name="formAdd" method="post" action="#" enctype="multipart/form-data">
                 <div class="radio-btn">
                   <p>Vous êtes :</p>
                   <div class="wrap-radio">
-                    <input
-                      type="radio"
-                      id="recruiter"
-                      name="role"
-                      value="recruiter"
-                      checked
-                    />
+                    <input type="radio" id="recruiter" name="role" value="recruiter" <?php if ($strUserType == "recruiter") {echo "checked";} ?> />
                     <label for="recruiter">Recruteur</label>
                   </div>
                   <div class="wrap-radio">
-                    <input
-                      type="radio"
-                      id="candidate"
-                      name="role"
-                      value="candidate"
-                    />
+                    <input type="radio" id="candidate" name="role" value="candidate" <?php if ($strUserType == "candidate") {echo "checked";} ?> />
                     <label for="candidate">Candidat</label>
                   </div>
                 </div>
                 <div class="style">
-                  <label for="name">Nom</label>
-                  <input type="text" id="name" />
+                  <label for="name"><?php echo $strName ?></label>
+                  <input type="text" id="name" name="name" />
                 </div>
                 <div class="style">
-                  <label for="firstName">Prénom</label>
-                  <input type="text" id="firstName" />
+                  <label for="firstName"><?php echo $strFirstname ?></label>
+                  <input type="text" id="firstName" name="firstname" />
                 </div>
                 <div class="style">
-                  <label for="adress">Adresse</label>
-                  <input type="text" id="adress" />
+                  <label for="address"><?php echo $strAddress ?></label>
+                  <input type="text" id="address" name="address"/>
                 </div>
 
                 <div class="style">
-                  <label for="email">Email</label>
-                  <input type="email" id="email" />
+                  <label for="email"><?php echo $strMail ?></label>
+                  <input type="email" id="email" name="email"/>
                 </div>
                 <div class="style">
-                  <label for="passWord">Mot de passe</label>
-                  <input type="password" id="passWord" />
+                  <label for="password"><?php echo $strPassword ?></label>
+                  <input type="password" id="password" name="password"/>
                 </div>
                 <button class="btn-subscribe style-button" type="submit">
                   S'incrire
