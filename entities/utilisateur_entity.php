f403b6b7e00be6ea3fb8489f25a27a8aee4bded8
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-01-31 22:19:00 +0100

    Correctifs apportés aux getters

--- entities/utilisateur_entity.php
+++ entities/utilisateur_entity.php
@@ -11,8 +11,8 @@
 		private $_utilisateur_date_creation;
 		private $_utilisateur_date_modification;
 		private $_utilisateur_date_connexion;
-        private $_ville_id;
-		private $_droit_id;
+        private $_utilisateur_ville_id;
+		private $_utilisateur_droit_id;
 		
 
 		public function __construct()
@@ -23,91 +23,91 @@
 			return $this->_utilisateur_id;
 		}
 		public function setUtilisateurId($intUtilisateur_id) {
-			return $this->_utilisateur_id;
+			$this->_utilisateur_id = $intUtilisateur_id;
 		}
 
 		public function getUtilisateurNom() {
-			return $this->$_utilisateur_nom;
+			return $this->_utilisateur_nom;
 		}
 		public function setUtilisateurNom($strUtilisateur_nom) {
-			return $this->$_utilisateur_nom;
+			$this->_utilisateur_nom = $strUtilisateur_nom;
 		}
 
 		public function getUtilisateurPrenom() {
-			return $this->$_utilisateur_prenom;
+			return $this->_utilisateur_prenom;
 		}
 		public function setUtilisateurPrenom($strUtilisateur_prenom) {
-			return $this->$_utilisateur_prenom;
+			$this->_utilisateur_prenom = $$strUtilisateur_prenom;
 		}
 
 		public function getUtilisateurAdresse() {
 			return $this->_utilisateur_adresse;
 		}
 		public function setUtilisateurAdresse($strUtilisateur_adresse) {
-			return $this->_utilisateur_adresse;
+			$this->_utilisateur_adresse = $strUtilisateur_adresse;
 		}
 
 		public function getUtilisateurType() {
 			return $this->_utilisateur_type;
 		}
 		public function setUtilisateurType($strUtilisateur_type) {
-			return $this->_utilisateur_type;
+			$this->_utilisateur_type = $strUtilisateur_type;
 		}
 
 		public function getUtilisateurCv() {
 			return $this->_utilisateur_cv;
 		}
 		public function setUtilisateurCv($strUtilisateur_cv) {
-			return $this->_utilisateur_cv;
+			$this->_utilisateur_cv = $strUtilisateur_cv;
 		}
 
 		public function getUtilisateurMail() {
 			return $this->_utilisateur_mail;
 		}
 		public function setUtilisateurMail($strUtilisateur_mail) {
-			return $this->_utilisateur_mail;
+			$this->_utilisateur_mail = $strUtilisateur_mail;
 		}
 
 		public function getUtilisateurMdp() {
 			return $this->_utilisateur_mdp;
 		}
 		public function setUtilisateurMdp($strUtilisateur_mdp) {
-			return $this->_utilisateur_mdp;
+			$this->_utilisateur_mdp = $strUtilisateur_mdp;
 		}
 
 		public function getUtilisateurDateCreation() {
 			return $this->_utilisateur_date_creation;
 		}
 		public function setUtilisateurDateCreation($strUtilisateur_date_creation) {
-			return $this->_utilisateur_date_creation;
+			$this->_utilisateur_date_creation = $strUtilisateur_date_creation;
 		}
 
 		public function getUtilisateurDateModification() {
-			return $this->__utilisateur_date_modification;
+			return $this->_utilisateur_date_modification;
 		}
 		public function setUtilisateurDateModification($strUtilisateur_date_modification) {
-			return $this->_utilisateur_date_modification;
+			$this->_utilisateur_date_modification = $strUtilisateur_date_modification;
 		}
 
 		public function getUtilisateurDateConnexion() {
 			return $this->_utilisateur_date_connexion;
 		}
 		public function setUtilisateurDateConnexion($strUtilisateur_date_connexion) {
-			return $this->_utilisateur_date_connexion;
+			$this->_utilisateur_date_connexion = $strUtilisateur_date_connexion;
 		}
 
 		public function getUtilisateurVilleId() {
 			return $this->_utilisateur_ville_id;
 		}
 		public function setUtilisateurVilleId($intUtilisateur_ville_id) {
-			return $this->_utilisateur_ville_id;
+			$this->_utilisateur_ville_id = $intUtilisateur_ville_id;
 		}
 
         public function getUtilisateurDroitId() {
 			return $this->_utilisateur_droit_id;
 		}
 		public function setUtilisateurDroitId($intUtilisateur_droit_id) {
-			return $this->_utilisateur_droit_id;
+			$this->_utilisateur_droit_id = $intUtilisateur_droit_id;
 		}
 	} 
 ?>
