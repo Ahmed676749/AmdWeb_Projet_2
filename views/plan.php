3905d4445c21a3bdefa5beaa1bbb47323bea3d2c
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-02-08 10:23:50 +0100

    Migration en vue

---
+++ views/plan.php
@@ -0,0 +1,33 @@
+	<main>
+		<!--Section plan du site-->
+		<div class="wrapper-list">
+			<h1 class="style-h">Plan du site</h1>
+			<ul>
+				<li>
+					<a href="index.php" class="welcome">Accueil</a>
+				</li>
+				<li>
+					<a href="index.php?ctrl=page&action=emploi">Emploi</a>
+				</li>
+				<li>
+					<a href="../job/details-emploi.php" class="details">Détails de l'offre d'emploi</a>
+				</li>
+				<li>
+					<a href="index.php?ctrl=utilisateur&action=inscription">Inscription</a>
+				</li>
+				<li>
+					<a href="index.php?ctrl=utilisateur&action=connexion">Connexion</a>
+				</li>
+				<li>
+					<a href="index.php?ctrl=page&action=galerie">Galerie</a>
+				</li>
+				<li>
+					<a href="index.php?ctrl=page&action=mentions">Mentions légales</a>
+				</li>
+				<li>
+					<a href="index.php?ctrl=page&action=plan">Plan du site</a>
+				</li>
+			</ul>
+		</div>
+	</main>
+</div>
