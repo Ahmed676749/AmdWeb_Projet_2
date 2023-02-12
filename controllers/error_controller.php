d5548f8ac4baeb30c8870af4c55f0d2c90f85f9e
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-02-12 18:17:13 +0100

    Update Add comments

--- controllers/error_controller.php
+++ controllers/error_controller.php
@@ -4,18 +4,25 @@
 	* @autor Jonathan Legrand
 	*/
 	class Error_controller extends Base_controller{
-		
+		/**
+		* Redirection vers la Page Error_404
+		*/
 		public function error_404(){
 			//Affichage
 			$this->_arrData['strTitle']	= "Page non trouvée";
 			$this->_arrData['strPage']	= "error_404";
+
 			$this->display("error_404");
 		}
 		
+		/**
+		* Redirection vers la Page Error_403
+		*/
 		public function error_403(){
 			//Affichage
 			$this->_arrData['strTitle']	= "Accès interdit";
 			$this->_arrData['strPage']	= "error_403";
+			
 			$this->display("error_403");
 		}		
 	}
