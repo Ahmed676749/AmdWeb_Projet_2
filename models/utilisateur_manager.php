bdfa1b2ce49a9d174b867025957492a444c44d0d
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-02-12 18:15:54 +0100

    Update requêtes préparées

--- models/utilisateur_manager.php
+++ models/utilisateur_manager.php
@@ -1,6 +1,5 @@
 <?php
-
-	require_once("connect.php");//Classe mère des managers
+	require_once("connect.php");	//Classe mère des managers
 	/**
 	* Class manager des utilisateurs
 	* @creator Jonathan Legrand
@@ -16,27 +15,36 @@
 		/**
 		* Méthode de récupération des utilisateurs
 		* @return array Liste des utilisateurs
-		* @return array Liste des utilisateurs
-
 		*/
 		public function afficherUtilisateur(){
 			$strRqUsers = "SELECT utilisateur_type, utilisateur_nom, utilisateur_prenom, utilisateur_adresse, utilisateur_mail, utilisateur_mdp FROM utilisateur;";
-							
+
+			var_dump($strRqUsers);
+						
 			return $this->_db->query($strRqUsers)->fetchAll();
 		}
 
 		/**
 		* Méthode de création des utilisateurs
 		*/
-		public function creerUtilisateur($objUser){
+		public function creerUtilisateur($objUserToAdd){
 			$strRqAdd 	= "INSERT INTO utilisateur 
 								(utilisateur_nom, utilisateur_prenom, utilisateur_adresse, utilisateur_mail, utilisateur_mdp, utilisateur_type, utilisateur_date_creation)
 							VALUES 
-								('".$objUser->getNom()."','".$objUser->getPrenom()."','".$objUser->getAdresse()."','".$objUser->getMail()."','".$objUser->getMdp()."','".$objUser->getType()."', NOW());";
+								(:nom, :prenom, :adresse, :mail, :mdp, :type, NOW())";
+			$prep		= $this->_db->prepare($strRqAdd);
 
-			var_dump($strRqAdd);
-			var_dump($objUser);
-			return $this->_db->exec($strRqAdd);
+			$prep->bindValue(':nom', $objUserToAdd->getNom(), PDO::PARAM_STR);
+			$prep->bindValue(':prenom', $objUserToAdd->getPrenom(), PDO::PARAM_STR);
+			$prep->bindValue(':adresse', $objUserToAdd->getAdresse(), PDO::PARAM_STR);
+			$prep->bindValue(':mail', $objUserToAdd->getMail(), PDO::PARAM_STR);
+			$prep->bindValue(':mdp', $objUserToAdd->getMdp(), PDO::PARAM_STR);
+			$prep->bindValue(':typeUtil', $objUserToAdd->getType(), PDO::PARAM_STR);
+
+			// var_dump($objUserToAdd);
+			$prep->execute();
+			var_dump($prep);
+			// return $this->_db->exec($strRqAdd);
 		}
 		
 		
