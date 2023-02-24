4389d116dded2590cc8d6bbca8ef21fe8e9d4cb4
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-02-24 13:57:46 +0100

    Create ville_manager.php

---
+++ models/ville_manager.php
@@ -0,0 +1,20 @@
+<?php
+	require_once("connect.php");	//Classe mère des managers
+	/**
+	* Class manager des villes
+	* @creator Jonathan Legrand
+	*/
+	class VilleManager extends Manager{
+		/**
+		* Constructeur de la classe
+		*/
+		public function __construct(){
+			parent::__construct();
+		}
+		
+		/**
+		* Méthode de 
+		* @return array 
+		*/
+		// public function getVille(){}
+    }
