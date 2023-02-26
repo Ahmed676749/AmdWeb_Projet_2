215db02652e2d18710956290567adab085019d8b
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-02-26 14:27:25 +0100

    Save

--- connect.php
+++ connect.php
@@ -16,7 +16,7 @@
 				$this->_db = new PDO(
 					"mysql:host=localhost;dbname=amd_web",  				// Serveur et BDD
 					"root",  												//Nom d'utilisateur de la base de données
-					"AmdWeb*68",	 													// Mot de passe de la base de données
+					"",	 													// Mot de passe de la base de données
 					array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC) // Mode de renvoi 
 					); 
 				// Pour résoudre les problèmes d’encodage
