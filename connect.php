b9e6cec1715d8953047cd58c2f7eb4c4a8c5bf51
Ahmed676749 <116066743+Ahmed676749@users.noreply.github.com>
2023-03-01 14:41:11 +0100

    blanc

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+	try{
+		// Connexion à la base de données
+		$db = new PDO(
+			"mysql:host=localhost;dbname=amd_web",  // Serveur et BDD
+			"root",  								//Nom d'utilisateur de la base de données
+			"",	 									// Mot de passe de la base de données
+			array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC) // Mode de renvoi 
+			); 
+		// Pour résoudre les problèmes d’encodage
+		$db->exec("SET CHARACTER SET utf8"); 	
+		// Configuration des exceptions
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+	} catch(PDOException $e) { 
+		echo "Échec : " . $e->getMessage(); 
+	}
---
+++ inscription.php
@@ -0,0 +1,74 @@
+<?php
+  $strTitle = "AmdWeb offres d'emplois, inscription";
+  $strPage = "inscription";
+
+  include("header.php");
+
+?>
+
+      <main>
+        <!--Section inscription-->
+        <section>
+          <div class="wrapper-subscribe">
+            <h1 class="style-h">Inscrivez vous</h1>
+            <p class="text-presentation">
+              Afin de déposer une offre d'emploi ou de postuler, vous devez vous
+              s'inscire.
+              <br />
+              Oui, l'inscription est obligatoire comme sur tous les sites.
+            </p>
+            <div class="form-box">
+              <form>
+                <div class="radio-btn">
+                  <p>Vous êtes :</p>
+                  <div class="wrap-radio">
+                    <input
+                      type="radio"
+                      id="recruiter"
+                      name="role"
+                      value="recruiter"
+                      checked
+                    />
+                    <label for="recruiter">Recruteur</label>
+                  </div>
+                  <div class="wrap-radio">
+                    <input
+                      type="radio"
+                      id="candidate"
+                      name="role"
+                      value="candidate"
+                    />
+                    <label for="candidate">Candidat</label>
+                  </div>
+                </div>
+                <div class="style">
+                  <label for="name">Nom</label>
+                  <input type="text" id="name" />
+                </div>
+                <div class="style">
+                  <label for="firstName">Prénom</label>
+                  <input type="text" id="firstName" />
+                </div>
+                <div class="style">
+                  <label for="adress">Adresse</label>
+                  <input type="text" id="adress" />
+                </div>
+
+                <div class="style">
+                  <label for="email">Email</label>
+                  <input type="email" id="email" />
+                </div>
+                <div class="style">
+                  <label for="passWord">Mot de passe</label>
+                  <input type="password" id="passWord" />
+                </div>
+                <button class="btn-subscribe style-button" type="submit">
+                  S'incrire
+                </button>
+              </form>
+            </div>
+          </div>
+        </section>
+      </main>
+      <?php include("footer.php") ?>
+    
